2b20e9997288531c258600e9733efb2ea89a7c1d
Nahuel Barrios <barrios.nahuel@gmail.com>
2013-11-05 23:59:45 -0300

    Add piseis.js and piseis-jquery-plugin.js with hello world functionality. It can be tested in index.html.

---
+++ src/plugin/piseis-jquery-plugin.js
@@ -0,0 +1,40 @@
+/*
+ * PiSeis - What people around the world is saying, you've got it.
+ *  Copyright (C) 2013 Nahuel Barrios <barrios.nahuel@gmail.com>.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Created by Nahuel Barrios <barrios.nahuel@gmail.com>.
+ * Created on 11/5/13, at 11:24 PM.
+ */
+
+(function ($) {
+
+    $.fn.piseis = function (options) {
+
+        //  This is the easiest way to have default options.
+        var settings = $.extend({
+                                    //  These are the defaults.
+                                    query: 'NBA',
+                                    excludeNetworks: ['Facebook']
+                                }, options);
+
+        org.nbempire.js.piseis.find(settings);
+
+        return this;
+    };
+
+}(jQuery));
