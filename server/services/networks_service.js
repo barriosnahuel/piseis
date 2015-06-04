5862905776a13137c1b5a90c28a228cf2d3016b6
Nahuel Barrios <barrios.nahuel@gmail.com>
2015-06-04 00:04:10 -0300

    Added Instagram to /networks resource. Still have to fix logo_url attribute (#32)

--- server/services/networks_service.js
+++ server/services/networks_service.js
@@ -2,13 +2,20 @@
  * Created by Nahuel Barrios on 25/05/15.
  */
 
+var RESOURCES_LOCATION = 'http://localhost:3000/public';
+
 exports.findAll = function () {
     return [
         {
-            id: 'flickr',
-            name: 'Flickr',
-            url: 'https://www.flickr.com/',
-            logo_url: 'https://s.yimg.com/pw/images/goodies/white-small-circle.png'
+            id: 'flickr'
+            , name: 'Flickr'
+            , url: 'https://www.flickr.com/'
+            , logo_url: 'https://s.yimg.com/pw/images/goodies/white-small-circle.png'
+        }, {
+            id: 'instagram'
+            , name: 'Instagram'
+            , url: 'https://www.instagram.com/'
+            , logo_url: RESOURCES_LOCATION + '/images' + '/network_logo_instagram.png'
         }
     ]
 };
